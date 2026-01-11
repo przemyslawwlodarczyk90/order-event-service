88dec74e4f720e3fd02f8483b2bb5260818b36ca
przemyslawwlodarczyk90 <przemyslawwlodarczyk90@wp.pl>
2026-01-11 12:02:10 +0100

    Initial commit

---
+++ src/test/java/com/example/order_event_service/OrderEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_event_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
