4666814c3df9bec0d4e1d4d4291902dd7dd95764
przemek <przemyslawwlodarczyk90@wp.pl>
2026-01-13 20:30:02 +0100

    diagramAplikacji.md

--- src/main/java/com/example/order_event_service/notification/handler/EmailNotificationHandler.java
+++ src/main/java/com/example/order_event_service/notification/handler/EmailNotificationHandler.java
@@ -1,4 +1,8 @@
 package com.example.order_event_service.notification.handler;
 
+import com.example.order_event_service.entity.OrderEvent;
+
 public interface EmailNotificationHandler {
+
+    void send(OrderEvent event);
 }
